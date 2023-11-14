8f45e462bea004316dd5ed9622bb1d2d77416d32
Roman Ptashnyk <2508romaa@gmail.com>
2023-11-14 16:22:38 +0200

    fix merge conflict

---
+++ dist/src/api/selected-bases-of-practice/controllers/selected-bases-of-practice.js
@@ -0,0 +1,7 @@
+"use strict";
+/**
+ * selected-bases-of-practice controller
+ */
+Object.defineProperty(exports, "__esModule", { value: true });
+const strapi_1 = require("@strapi/strapi");
+exports.default = strapi_1.factories.createCoreController('api::selected-bases-of-practice.selected-bases-of-practice');
